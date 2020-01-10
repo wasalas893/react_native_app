d0932684f768129c37a70e17891abe403d5768d6
wasalas <wasalas893@gmail.com>
2020-01-10 17:50:32 +0530

    result list get the api data

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useEffect,useState } from 'react';
 
 import {View,StyleSheet,Text}from 'react-native';
+import yelp from '../api/yelp';
 
-const ResultsShowScreen=()=>{
+const ResultsShowScreen=({navigation})=>{
+
+    const [result,setResult]=useState(null);
+    const id=navigation.getParam('id');
+
+    console.log(result);
+
+    const getResult=async id=>{
+        const response=await yelp.get(`/${id}`);
+       setResult(response.data);
+    };
+
+    useEffect(()=>{
+        getResult(id);
+
+    },[]);
+
     return(
         <View>
             <Text>Result show list</Text>
